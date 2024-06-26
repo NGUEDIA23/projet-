a52b4b5a06a592cb37f36e5f1a7fe4b9c4787a99
NGUEDIA23 <138587011+NGUEDIA23@users.noreply.github.com>
2024-06-27 01:07:39 +0200

    add multilang_site

--- multilang_site/main/views.py
+++ multilang_site/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from .models import Article
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'main/article_list.html', {'articles': articles})
+
