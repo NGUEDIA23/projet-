438cfb144d51991b9fb856fd17d831ba949dfb02
NGUEDIA23 <138587011+NGUEDIA23@users.noreply.github.com>
2024-06-29 20:25:19 +0200

    add multilang_site

--- multilang_site/multilang_site/urls.py
+++ multilang_site/multilang_site/urls.py
@@ -1,19 +1,5 @@
-"""
-URL configuration for multilang_site project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Inclure les URLs de votre application principale
+    # Ajoutez d'autres URLs internationalisées si nécessaire
 from django.contrib import admin
 from django.urls import path, include
 from main.views import home 
@@ -24,6 +10,5 @@ urlpatterns = [
     path('', home, name='home'), 
 ]
 urlpatterns += i18n_patterns(
-    path('articles/', include('main.urls')),  # Inclure les URLs de votre application principale
-    # Ajoutez d'autres URLs internationalisées si nécessaire
+    path('articles/', include('main.urls')), 
 )
